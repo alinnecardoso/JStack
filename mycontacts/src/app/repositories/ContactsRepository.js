2025c69908b5d02914468c77812b90f71e6193b4
alinnecardoso <dev14cardoso@gmail.com>
2024-08-05 15:34:07 -0300

    uuid problem fixed

--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,12 +1,20 @@
+const { v4 } = require('uuid');
+
 const contacts = [
   {
-    id:""
+    id: v4(),
+    name: 'Alinne',
+    email: 'alinne@gmail.com',
+    phone: '123456789',
+    category_id: v4(),
   }
 ];
 
 class ContactsRepository{
   findAll(){
-
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
   }
 }
 

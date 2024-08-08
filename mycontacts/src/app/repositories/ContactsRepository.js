9ec1596391162c9fcaab0d9f5ea378c852bfb4d4
alinnecardoso <dev14cardoso@gmail.com>
2024-08-08 17:58:16 -0300

    "Removed delete method, updated create and update methods to use database queries, and readded delete method with a new implementation"

--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -34,12 +34,6 @@ class ContactsRepository{
     const [rows] = await db.query('SELECT * FROM contacts WHERE email=$1', [email]);
     return rows;
   }
-  delete(id){
-    return new Promise((resolve)=>{
-      contacts = contacts.filter((contact) => contact.id !== id),
-      resolve();
-    });
-  }
   async create({name, email, phone, category_id}){
     const [row] = await db.query(
       `INSERT INTO contacts(name, email, phone, category_id)
@@ -51,17 +45,19 @@ class ContactsRepository{
     return row;
   }
 
-  update(id, {name, email, phone, category_id}){
+  async update(id, {name, email, phone, category_id}){
+    const [row] = await db.query(`
+          UPDATE contacts
+          SET name = $1, email = $2, phone = $3, category_id = $4
+          WHERE id = $5
+          RETURNING *
+      `, [name, email, phone, category_id, id]);
+    return row
+  }
+  delete(id){
     return new Promise((resolve)=>{
-      const updatedContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      }
-      contacts = contacts.map((contact) => contact.id === id ? updatedContact : contact);
-      resolve(updatedContact);
+      contacts = contacts.filter((contact) => contact.id !== id),
+      resolve();
     });
   }
 }

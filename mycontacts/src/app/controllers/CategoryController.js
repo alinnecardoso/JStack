3ab1c0019dbbc8868affd1a9d29bb99267d52247
alinnecardoso <dev14cardoso@gmail.com>
2024-08-09 16:51:38 -0300

    "Added category joins to ContactsRepository and added CategoryController routes"

---
+++ mycontacts/src/app/controllers/CategoryController.js
@@ -0,0 +1,24 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository')
+
+class CategoryController {
+  async index(request, response){
+    //Listar todos os registros
+    const categories = await CategoriesRepository.findAll();
+
+    response.json(categories);
+  }
+
+  //Criar um novo registro
+  async store(request, response){
+    const { name } = request.body;
+
+    if(!name){
+      return response.status(400).json({error: 'Name is required'})
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+}
+module.exports = new CategoryController();

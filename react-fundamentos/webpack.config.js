efc18c0481e42e11ec5348a552d724d3f14ca9c0
alinnecardoso <dev14cardoso@gmail.com>
2024-08-16 17:36:08 -0300

    Gerando o Bundle com o WebPack

---
+++ react-fundamentos/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports ={
+  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[fullhash].js',
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}

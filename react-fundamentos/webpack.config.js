a4bd6f8f5cf487f9218d7e4c8904d38187f0e582
alinnecardoso <dev14cardoso@gmail.com>
2024-08-16 17:51:39 -0300

    Configuração webpack

--- react-fundamentos/webpack.config.js
+++ react-fundamentos/webpack.config.js
@@ -3,7 +3,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports ={
-  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle[fullhash].js',

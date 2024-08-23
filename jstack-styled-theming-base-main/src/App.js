6f67db0c8e80b56f2a1f3fd8f918edb4ea3bcb36
alinnecardoso <dev14cardoso@gmail.com>
2024-08-23 17:52:21 -0300

    Projeto de mudança de tema - Aula

---
+++ jstack-styled-theming-base-main/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useMemo } from 'react';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+import { ThemeProvider } from 'styled-components';
+
+import themes from './styles/themes'
+
+const [ theme, setTheme ] = useState('dark');
+
+const currentTheme = useMemo(() => {
+  return themes[theme] || themes.dark;
+}, [theme]);
+
+function handleToggleTheme(){
+  setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={currentTheme} >
+      <GlobalStyle />
+      <Layout 
+      onToggleTheme={handleToggleTheme}
+      selectedTheme={theme}
+      />
+    </ ThemeProvider>
+  );
+};
+
+export default App;

8e26a6695e16c3d419128092c4a561f0035daef3
alinnecardoso <dev14cardoso@gmail.com>
2024-09-12 12:26:17 -0300

    "Added Input, Select, and Button components"

--- mycontacts/fe/src/pages/NewContact/index.js
+++ mycontacts/fe/src/pages/NewContact/index.js
@@ -1,7 +1,28 @@
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
+import Select from "../../components/Select";
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Facebook</option>
+        <option value="123">Twitter</option>
+      </Select>
+
+      <Button type="button" >
+        Salvar Alterações
+      </Button>
+
+      <Button type="button" disabled>
+        Salvar Alterações
+      </Button>
+    </>
   )
 }

3b4d95b408a2d2cf63febb0b8c439f22e2d17d58
alinnecardoso <dev14cardoso@gmail.com>
2024-09-12 16:08:45 -0300

    "Updated Button component, added styles to ContactForm, and added Modal component to Home page"

--- mycontacts/fe/src/components/ContactForm/styles.js
+++ mycontacts/fe/src/components/ContactForm/styles.js
@@ -6,4 +6,8 @@ export const Form = styled.div`
 
 export const ButtonContainer = styled.div`
   margin-top: 24px;
+
+  button{
+    width:100%;
+  }
 `;
---
+++ mycontacts/fe/src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import Button from "../Button";
+import { Container, Overlay, Footer } from "./styles";
+
+export default function Modal(){
+  return (
+    <Overlay>
+      <Container>
+        <h1>Título Modal</h1>
+        <p>
+          Corpo do modal
+        </p>
+
+        <Footer>
+          <button type="button" className="cancel-button" >Cancelar</button>
+
+          <Button type="button" >
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
+    </Overlay>
+  )
+}

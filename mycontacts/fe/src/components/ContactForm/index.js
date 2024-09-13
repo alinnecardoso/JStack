72634ff5293a51b5f7953edc8bebce9d0b264f50
alinnecardoso <dev14cardoso@gmail.com>
2024-09-13 16:27:06 -0300

    "Updated index.html title and modified ContactForm component to add more fields, validation, and event handlers."

--- mycontacts/fe/src/components/ContactForm/index.js
+++ mycontacts/fe/src/components/ContactForm/index.js
@@ -7,35 +7,79 @@ import { Form, ButtonContainer } from "./styles";
 import PropTypes from 'prop-types'
 
 export default function ContactForm({ buttonLabel }){
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [ errors, setErrors ] = useState([])
+
+  function handleNameChange(event){
+    setName(event.target.value);
+
+    if(!event.target.value){
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatório.'}
+      ])
+    }else{
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'name' 
+      ))
+    }
+  }
+
+  console.log(errors)
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    console.log({
+      name,
+      email,
+      phone,
+      category
+    })
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit} >
       <FormGroup>
         <Input
-          value={name}
           placeholder="Nome"
-          onChange={(event)=> setName(event.target.value)}
+          value={name}
+          onChange={handleNameChange}
         />
       </FormGroup>
 
-      <FormGroup
-        error="O formato do e-mail é invalido"
-      >
-        <Input placeholder="E-mail" error />
+      <FormGroup>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event)=> setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event)=> setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event)=> setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit" >
+        <Button type="submit" onClick={handleSubmit} >
           {buttonLabel}
         </Button>
       </ButtonContainer>

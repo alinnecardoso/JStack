734ca571e59b52a0304b7e17e2e3fe8c217d1838
Alinne <alinne14cardoso@gmail.com>
2025-02-25 13:36:01 -0300

    `feat: Add toast functionality with event manager and remove message functionality`

--- mycontacts/fe/src/components/Toast/ToastContainer/index.js
+++ mycontacts/fe/src/components/Toast/ToastContainer/index.js
@@ -1,27 +1,31 @@
 import { useState, useEffect } from "react";
 import { Container } from "./styles";
 import ToastMessage from "../ToastMessage";
+import { toastEventManager } from "../../../utils/toast";
 
 
 export default function ToastContainer() {
   const [ messages, setMessages ] = useState([])
 
   useEffect(() => {
-    function handleAddToast(event){
-      const { type, text } = event.detail;
-
+    function handleAddToast({  type, text}){
       setMessages((prevState) => [
         ...prevState,
         { id:Math.random(), type, text }
       ]);
     }
 
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
 
     return () => {
-      document.removeEventListener('addtoast', handleAddToast)
+      toastEventManager.removeListener('addtoast', handleAddToast)
     }
   }, [])
+
+  function handleRemoveMessage(){
+    console.log('ToastContainer -> hanldeRemoveMessage')
+  }
+
   return (
     <Container>
       { messages.map((message) => (
@@ -29,6 +33,7 @@ export default function ToastContainer() {
           key={message.id}
           type={message.type}
           text={message.text}
+          onRemoveMessage={handleRemoveMessage}
         />
       )) }
     </Container>
--- mycontacts/fe/src/components/Toast/ToastMessage/index.js
+++ mycontacts/fe/src/components/Toast/ToastMessage/index.js
@@ -5,9 +5,12 @@ import PropTypes from "prop-types";
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg'
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg'
 
-export default function ToastMessage({ text, type }){
+export default function ToastMessage({ text, type, onRemoveMessage }){
+  function handleRemoveToast(){
+    onRemoveMessage('message-id')
+  }
   return (
-    <Container type={type} >
+    <Container type={type} onClick={handleRemoveToast} >
       { type === 'danger' && <img src={xCircleIcon} alt="X" /> }
       { type === 'success' && <img src={checkCircleIcon} alt="Check" /> }
       <strong>{ text }</strong>
@@ -17,7 +20,8 @@ export default function ToastMessage({ text, type }){
 
 ToastMessage.propTypes = {
   text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf([ 'default', 'success', 'danger' ])
+  type: PropTypes.oneOf([ 'default', 'success', 'danger' ]),
+  onRemoveMessage: PropTypes.func.isRequired
 }
 
 ToastMessage.defaultTypes = {
